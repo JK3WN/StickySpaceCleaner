f1da64b0754813ef08672548b55487783cce7f72
JK3WN <yimjay1@gmail.com>
2024-07-21 20:42:46 +0900

    Added Stick Mechanism

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -9,12 +9,22 @@ public class Stick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("DebrisFree"))
+        {
+            collision.transform.SetParent(this.transform);
+            collision.gameObject.tag = new string("DebrisStuck");
+            collision.gameObject.AddComponent<Stick>();
+        }
     }
 }
