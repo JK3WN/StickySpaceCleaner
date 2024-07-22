02141cf36623ab3c1e2cf34fe5f814ebb8588176
JK3WN <yimjay1@gmail.com>
2024-07-22 10:25:50 +0900

    Added LeaderBoard

--- Assets/Scripts/MenuManagement.cs
+++ Assets/Scripts/MenuManagement.cs
@@ -5,16 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuManagement : MonoBehaviour
 {
+    public TMPro.TextMeshProUGUI HighScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("HighScore"))
+        {
+            PlayerPrefs.SetString("HighScore", "0\n\n0\n\n0\n\n0\n\n0");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HighScoreText.text = PlayerPrefs.GetString("HighScore");
     }
 
     public void StartPressed()
